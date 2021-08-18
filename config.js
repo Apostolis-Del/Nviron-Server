225f8d56f8fbf096023550ab29af07083e05d0ae
Apostolis-Del <del13killer@gmail.com>
2021-08-18 17:30:35 +0300

    kati ksexasa

---
+++ config.js
@@ -0,0 +1,5 @@
+//for connecting to atlas database
+module.exports = {
+    MONGODB: 'mongodb+srv://del:512290799@cluster0.7w3jl.mongodb.net/Communite?retryWrites=true&w=majority',
+    SECRET_KEY:'some very secret key'
+}
